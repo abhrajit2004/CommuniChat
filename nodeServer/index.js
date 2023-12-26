239ab2e4749b90d9e16b3cbc3699908ba08f14a7
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2023-12-26 23:15:28 +0530

    First commit

---
+++ nodeServer/index.js
@@ -0,0 +1,20 @@
+const io = require("socket.io")(8000);
+
+const users = {};
+
+io.on('connection',socket=>{
+    socket.on('new-user-joined',username=>{
+        console.log("New User ",username);
+        users[socket.id] = username;
+        socket.broadcast.emit('user-joined', username);
+    });
+
+    socket.on('send',message=>{
+        socket.broadcast.emit('receive',{message: message, username:users[socket.id]});
+    });
+
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
+    });
+})
