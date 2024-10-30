6b8aa708c3402c7faf899b4c013fec031d2c2130
unknown <abhrajitgupta.it2022@nsec.ac.in>
2024-10-30 21:19:43 +0530

    modified website

--- nodeServer/index.js
+++ nodeServer/index.js
@@ -1,19 +1,27 @@
 const io = require("socket.io")(8000);
+const app = require('express')();
+const cors = require('cors');
+
+app.use(cors());
 
 const users = {};
 
-io.on('connection',socket=>{
-    socket.on('new-user-joined',username=>{
+io.on('connection', socket => {
+    socket.on('new-user-joined', username => {
         users[socket.id] = username;
         socket.broadcast.emit('user-joined', username);
     });
 
-    socket.on('send',message=>{
-        socket.broadcast.emit('receive',{message: message, username:users[socket.id]});
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, username: users[socket.id] });
     });
 
-    socket.on('disconnect', ()=>{
-        socket.broadcast.emit('left',users[socket.id]);
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('left', users[socket.id]);
         delete users[socket.id];
     });
+})
+
+app.listen(() => {
+    console.log('Server is running on port 8000');
 })
